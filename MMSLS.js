3ac7c51cf9ba426b64595341eb02a34a10105341
xi-kop1208 <151592994+xi-kop1208@users.noreply.github.com>
2023-12-02 17:46:48 +0800

    Update MMSLS.js
    20231202
    https://www.maimemo.com/share/page?uid=32578856&pid=f17f94085b9d7c498e8d2f0cf69dc256&tid=cf4b2dfb9ecce94696f9afc9b1970bfe
    link updated

--- MMSLS.js
+++ MMSLS.js
@@ -9,7 +9,7 @@ const MMSLS = [
     "ww",    "https://www.maimemo.com/share/page?uid=32578856&pid=cf2908aca3f8357f701384ad1678328f&tid=995360cc58946ecdc38d673df29545e0",
     "wjl",
     "fy",
-    "https://www.maimemo.com/share/page?uid=15249640&pid=afaa99854200461684ec24c45b6aa6e4&tid=57653da5c7394f126d7923c9044b4dc1",
+    "https://www.maimemo.com/share/page?uid=32578856&pid=f17f94085b9d7c498e8d2f0cf69dc256&tid=cf4b2dfb9ecce94696f9afc9b1970bfe",
   ]
 =====================以上为范例！=====================
 
